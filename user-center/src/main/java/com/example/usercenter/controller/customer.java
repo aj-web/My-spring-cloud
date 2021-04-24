9d6d6ea99a77af329773ffd3e325bfcc2747dfa7
“you <you>
2021-04-24 17:54:10 +0800

    sentinel的简单使用

--- user-center/src/main/java/com/example/usercenter/controller/customer.java
+++ user-center/src/main/java/com/example/usercenter/controller/customer.java
@@ -1,5 +1,12 @@
 package com.example.usercenter.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.example.usercenter.openservice.OrderFeignService;
 import com.example.usercenter.service.RemoteService;
 import feign.Feign;
@@ -13,6 +20,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description:
@@ -62,5 +73,48 @@ public class customer {
     @RequestMapping(value = "/OpenFeignBuy/{num}")
     public String OpenFeignBuy(@PathVariable int num){
         return orderFeignService.OpenFeignBuy(num);
+    }
+
+    private static final String RESOURCE_NAME = "hello";
+
+    @RequestMapping("/helloSentinel")
+    public String hello() {
+        Entry entry = null;
+        try {
+
+            entry = SphU.entry(RESOURCE_NAME);
+            //业务逻辑
+            String s = "Hello Sentinel";
+            return s;
+
+        } catch (BlockException e) {
+            log.info("BLOCK!");
+        } catch (Exception e) {
+            Tracer.traceEntry(e, entry);
+        }finally {
+            if (entry != null) {
+                entry.exit();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 定义流控规则
+     */
+    @PostConstruct
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        //设置受保护的资源
+        rule.setResource(RESOURCE_NAME);
+        // 设置流控规则 QPS
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // 设置受保护的资源阈值
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        // 加载配置好的规则
+        FlowRuleManager.loadRules(rules);
     }
 }
