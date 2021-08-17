f8127483ae44a3d793378a259ebb426dd11d5737
“you <you>
2021-08-18 00:15:10 +0800

    nacos

--- product-center/src/main/java/com/example/productcenter/controller/Producter.java
+++ product-center/src/main/java/com/example/productcenter/controller/Producter.java
@@ -1,6 +1,5 @@
 package com.example.productcenter.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2021/4/8 16:55
  */
 @RestController
-@Slf4j
 @RequestMapping("/producter")
 public class Producter {
 
 
-    @RequestMapping(value = "/Buy/{num}")
+    @RequestMapping(value = "/buy/{num}")
     public String SellBangBangTang(@PathVariable int num){
         return "你已经买到了"+num+"根棒棒糖啦";
     }
